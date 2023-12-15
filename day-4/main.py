5ed050bf6e393fa9803ce548b5d143a70ced9b4f
pazag <paulz@pazag-11>
2023-12-15 13:12:22 -0500

    Day 4 - Part 2

--- day-4/main.py
+++ day-4/main.py
@@ -1,5 +1,6 @@
 # Standard library import
 import argparse
+import collections
 import logging
 import os
 
@@ -14,15 +15,30 @@ def main():
         logging.error(f"File {args.filename} does not exist.")
 
     total_part_1 = 0
+    total_part_2 = 0
+    nb_card_per_index = collections.defaultdict(lambda: 0)
     with open(args.filename, 'r') as f:
         lines = f.readlines()
-        for line in lines:
+        for i, line in enumerate(lines):
+            current_card = i + 1
+            nb_card_per_index[current_card] += 1
+
             all_cards = line.split(':')[1]
             eventual_winning_cards, cards_in_hand = [cards.split() for cards in all_cards.split('|')]
             winning_cards = list(set(eventual_winning_cards) & set(cards_in_hand))
+            # Part 1
             if winning_cards:
                 total_part_1 += pow(2, len(winning_cards) - 1)
-    print(total_part_1)
+            # Part 2
+            nb_winning_cards = len(winning_cards)
+            for next_card in range(current_card + 1, current_card + 1 + nb_winning_cards):
+                nb_card_per_index[next_card] += nb_card_per_index[current_card]
+
+    for number_card in nb_card_per_index.values():
+        total_part_2 += number_card
+        
+    print(f"total_part_1:{total_part_1}")
+    print(f"total_part_2:{total_part_2}")
 
 
 if __name__ == "__main__":

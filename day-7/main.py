46158e33cadbb8b79df2df988700eb1271e8e0e3
pazag <paulz@pazag-11>
2023-12-16 02:36:49 -0500

    Day 7 - Part 2

--- day-7/main.py
+++ day-7/main.py
@@ -13,12 +13,23 @@ def main():
     if not os.path.exists(args.filename):
         logging.error(f"File {args.filename} does not exist.")
 
-    card_strenght_order = ['A', 'K', 'Q', 'J', 'T'] 
+    print("------Part 1------")
+    _get_total_winnings(args.filename, False)
+
+    print("------Part 2------")
+    _get_total_winnings(args.filename, True)  
+
+
+def _get_total_winnings(filename:str,
+                        is_part_2:bool):
+    card_strenght_order = ['A', 'K', 'Q', 'J', 'T'] if not is_part_2 else ['A', 'K', 'Q', 'T']
     for i in range(9, 1, -1):
         card_strenght_order.append(str(i))
+    if is_part_2:
+        card_strenght_order.append('J')
     card_strenght_order = {card:(len(card_strenght_order)-strenght) for (strenght, card) in enumerate(card_strenght_order)}
 
-    with open(args.filename, 'r') as f:
+    with open(filename, 'r') as f:
         lines = f.readlines()
         cards_and_bid_list = []
         for line in lines:
@@ -27,6 +38,15 @@ def main():
             cards_as_list.sort(key=lambda x:(cards_in_hand.count(x), 
                                              card_strenght_order[x]))
             sorted_cards_in_hand = ''.join(cards_as_list)
+            # Part 2 - Replace 'J' by the strongest cards
+            if is_part_2:
+                cards_without_j = sorted_cards_in_hand.replace('J', '')
+                while len(cards_without_j) < 5:
+                    if not cards_without_j:
+                        cards_without_j = 'JJJJJ'
+                    else:
+                        cards_without_j += cards_without_j[-1]
+                sorted_cards_in_hand = cards_without_j
             # For each hand, keep the triplet (sorted cards, bid, initial cards) so that the
             # different hands are easier to sort
             cards_and_bid_list.append([sorted_cards_in_hand, int(bid), cards_in_hand])
@@ -44,7 +64,7 @@ def main():
         total = 0
         for i in range(len(cards_and_bid_list)):
             total += cards_and_bid_list[i][1] * (i + 1) 
-        print(total)        
+        print(f"total:{total}")        
 
 
 if __name__ == "__main__":

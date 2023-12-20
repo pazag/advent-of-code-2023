96e088f8ec997999e21e0899f577e563b64c0123
pazag <paulz@pazag-11>
2023-12-19 21:55:56 -0500

    Day 13 - Part 2

--- day-13/main.py
+++ day-13/main.py
@@ -17,6 +17,7 @@ def main():
         logging.error(f"File {args.filename} does not exist.")
 
     total_part_1 = 0
+    total_part_2 = 0
     patterns = []
     with open(args.filename, 'r') as f:
         lines = f.read().split('\n')
@@ -27,43 +28,55 @@ def main():
                 pattern_start = i + 1
     
     for pattern in patterns:
-        total_part_1 += _rows_above_horizontal_mirrors(pattern, True) * 100
-        total_part_1 += _rows_above_horizontal_mirrors(pattern, False) 
+        total_part_1 += _number_lines_before_mirrors(pattern, True, True) * 100
+        total_part_1 += _number_lines_before_mirrors(pattern, False, True) 
+
+        total_part_2  += _number_lines_before_mirrors(pattern, True, False) * 100
+        total_part_2 += _number_lines_before_mirrors(pattern, False, False) 
     print(f"total_part_1:{total_part_1}")
+    print(f"total_part_2:{total_part_2}")
 
 
-def _rows_above_horizontal_mirrors(matrix, 
-                                   iterate_over_rows):
+def _number_lines_before_mirrors(matrix, 
+                                 iterate_over_rows : bool,
+                                 zero_or_one_difference : bool):
+    ''' 
+    The parameter zero_or_one_difference indicates whether there must be zero
+    differences to find mirrors (true) or exactly one (false).
+    '''
     nb_line_next_to_mirror = 0
     N = matrix.shape[0] if iterate_over_rows else matrix.shape[1]
     for n in range(N - 1):
         array_1 = matrix[n] if iterate_over_rows else matrix[:, n]
         array_2 = matrix[n + 1] if iterate_over_rows else matrix[:, n + 1]
-        if _identical_arrays(array_1, array_2):
+        nb_diff = _nb_diff_between_arrays(array_1, array_2)
+        if (zero_or_one_difference and nb_diff == 0) or \
+           (not zero_or_one_difference and nb_diff <= 1):
             is_mirror = False
             for m in range(1, N):
                 if n - m < 0 or n + 1 + m >= N:
-                    is_mirror = True
+                    if zero_or_one_difference or (not zero_or_one_difference and nb_diff == 1):
+                        is_mirror = True
                     break
                 array_1 = matrix[n     - m] if iterate_over_rows else matrix[:, n     - m]
                 array_2 = matrix[n + 1 + m] if iterate_over_rows else matrix[:, n + 1 + m]
-                if not _identical_arrays(array_1, array_2):
+                nb_diff += _nb_diff_between_arrays(array_1, array_2)
+                if (zero_or_one_difference and nb_diff > 0) or \
+                   (not zero_or_one_difference and nb_diff > 1):
                     break
             if is_mirror:
                 nb_line_next_to_mirror += n + 1
     return nb_line_next_to_mirror
 
 
-def _identical_arrays(array_1,
-                      array_2):
-    if array_1.shape != array_2.shape:
-        return False
-    
+def _nb_diff_between_arrays(array_1,
+                            array_2)->int:
+    nb_diff = 0  
     for i in range(array_1.shape[0]):
         for j in range(array_1.shape[1]):
             if array_1[i,j] != array_2[i,j]:
-                return False
-    return True
+                nb_diff += 1
+    return nb_diff
 
 
 if __name__ == "__main__":

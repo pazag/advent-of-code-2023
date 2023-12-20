ca557db5cf21a6c41960892df2e8f38d56bf3156
pazag <paulz@pazag-11>
2023-12-20 18:07:18 -0500

    Day 14

--- day-14/main.py
+++ day-14/main.py
@@ -1,11 +1,13 @@
 # Standard library import
 import argparse
-import collections
 import logging
 import os
 
 # Third party import
 import numpy as np
+
+
+NB_CYCLE = 1000000000
 
 
 def main():
@@ -18,18 +20,48 @@ def main():
         logging.error(f"File {args.filename} does not exist.")
 
     total_part_1 = 0
+    total_part_2 = 0
     with open(args.filename, 'r') as f:
         lines = f.read().split()
         char_matrix = np.matrix([list(line) for line in lines])
-        for j in range(char_matrix.shape[1]):
-            last_possible_rock_row = 0
-            for i in range(char_matrix.shape[0]):
-                if char_matrix[i,j] == '#':
-                    last_possible_rock_row = i + 1
-                elif char_matrix[i,j] == 'O':
-                    total_part_1 += char_matrix.shape[0] - last_possible_rock_row
-                    last_possible_rock_row += 1        
-    print(total_part_1)
+        # Part 1
+        char_matrix_part_1 = _move_rocks_to_top(char_matrix)
+        total_part_1 += _calculate_load(char_matrix_part_1)
+        print(f"total_part_1:{total_part_1}")
+
+
+        # Part 2 (Does not scale)
+        for n in range(NB_CYCLE):
+            for i in range(4):
+                char_matrix = _move_rocks_to_top(char_matrix)
+                char_matrix = np.rot90(char_matrix, k=1, axes=(1,0))
+        total_part_2 += _calculate_load(char_matrix)
+        print(f"total_part_2:{total_part_2}")
+
+
+def _move_rocks_to_top(matrix)->np.matrix:
+    for j in range(matrix.shape[1]):
+        last_possible_rock_row = 0
+        for i in range(matrix.shape[0]):
+            ij = (i,j)
+            if matrix[ij] == '#':
+                last_possible_rock_row = i + 1
+            elif matrix[ij] == 'O':
+                matrix[last_possible_rock_row, j] = 'O'
+                if not i == last_possible_rock_row:
+                    matrix[ij] = '.'
+                last_possible_rock_row += 1
+    return matrix
+
+
+def _calculate_load(matrix):
+    print(matrix)
+    load = 0
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i,j] == 'O':
+                load += matrix.shape[0] - i
+    return load
 
 
 if __name__ == "__main__":
